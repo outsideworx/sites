8e644a71cce79887be7dcc5ee8343c439d82e416
Outside Worx <info@outsideworx.net>
2026-04-01 07:46:22 +0000

    Exit after letsencrypt.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ DEST="/home/outsideworx/sites"
 if [ "$1" == "--letsencrypt" ]; then
     # WARNING: For this section to work, port 80 has to be open and accessible via the below mentioned address.
     certbot certonly --standalone --noninteractive --agree-tos --email info@outsideworx.net -d sites.outsideworx.net
+    exit 0
 fi
 
 echo "Copying project files to: $DEST"
