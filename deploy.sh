2552b6813c730f13fa0890b9faec361307979c2f
Outside Worx <info@outsideworx.net>
2026-04-01 10:41:47 +0000

    deploy.sh 1

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 DEST="/home/outsideworx/sites"
 
+set -e
+
 if [ "$1" == "--letsencrypt" ]; then
     # WARNING: For this section to work, port 80 has to be open and accessible via the below mentioned address.
     certbot certonly --standalone --noninteractive --agree-tos --email info@outsideworx.net -d sites.outsideworx.net
@@ -20,12 +22,7 @@ if [ -n "$1" ]; then
     exit 1
 fi
 
-if [ ! -f "$SCRIPT_DIR/.env" ]; then
-    echo "Error: .env file not found"
-    exit 1
-fi
-
-echo "Copying project files to: $DEST"
+echo "Copying standalone project files to: $DEST."
 rm -rf "$DEST"
 mkdir -p "$DEST"
 cp "$SCRIPT_DIR/.env" \
@@ -34,11 +31,11 @@ cp "$SCRIPT_DIR/.env" \
    "$SCRIPT_DIR/Dockerfile" \
    "$DEST"
 
-echo "Deployment starts"
+echo "Container deployment starts."
 cd "$DEST"
 docker compose build --no-cache --pull
 docker compose up --force-recreate --no-deps -d
-echo "Sleep, to make sure everything is running"
+echo "Sleep, to make sure everything is running."
 sleep 10
 docker system prune -af
 docker stats
